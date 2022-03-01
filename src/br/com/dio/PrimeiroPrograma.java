add15acb51b542027c537a97c64cadcdbee36d26
Sonolex <heliohenriquemoraes@gmail.com>
2022-03-01 13:35:29 -0300

    feito

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,21 +1,55 @@
-package br.com.dio;
-
-import br.com.dio.model.Gato;
-
-public class PrimeiroPrograma {
-    public static void main(String[] args) {
-        Gato gato = new Gato();
-        System.out.println(gato);
-
-
-
-
-
-
-
-                                                  /*int a = 5;
-        int b = 3;
-        System.out.println("Hello World! " + (a+b));*/
-    }
-}
-
+//package br.com.dio;
+//
+//import br.com.dio.model.Gato;
+//
+//public class PrimeiroPrograma {
+//    public static void main(String[] args) {
+//        Gato gato = new Gato();
+//        System.out.println(gato);
+//
+//        Object nome;
+//
+//        System.out.println(livro1);
+//                                             /*int a = 5;
+//        int b = 3;
+//        System.out.println("Hello World! " + (a+b));*/
+//    }
+//
+//    private static class Livro {
+//    }
+//
+//
+//    class livro {
+//
+//    private String nome;
+//    private String numPaginas;
+//
+//    public livro(String nome, String numPaginas) {
+//        this.nome = nome;
+//        this.numPaginas = numPaginas;
+//    }
+//
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public String getNumPaginas() {
+//        return numPaginas;
+//    }
+//
+//    public void setNumPaginas(String numPaginas) {
+//        this.numPaginas = numPaginas;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "livro{" +
+//                "nome='" + nome + '\'' +
+//                ", numPaginas='" + numPaginas + '\'' +
+//                '}';
+//    }
+//}
